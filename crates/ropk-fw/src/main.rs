777b95efc8afcb3a8f502e9b2cecbde7b582306d
Mohit-Pala <vmohit.pala@gmail.com>
2026-07-25 17:06:51 -0400

    feat: research radio

--- crates/ropk-fw/src/main.rs
+++ crates/ropk-fw/src/main.rs
@@ -6,129 +6,33 @@ use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
 
 
-// stole examples from here
-// https://github.com/embassy-rs/embassy/blob/main/examples/nrf52840/src/bin/usb_hid_keyboard.rs
-use embassy_nrf::{bind_interrupts, peripherals, usb};
-use embassy_nrf::usb::Driver;
-use embassy_nrf::usb::vbus_detect::HardwareVbusDetect;
-use embassy_usb::class::hid::{Config as HidConfig, HidReaderWriter, State};
-use embassy_usb::Config;
-use static_cell::StaticCell;
-use usbd_hid::descriptor::{KeyboardReport, SerializedDescriptor};
 
-
-// todo:
-// research this 
-bind_interrupts!(struct Irqs {
-    USBD => usb::InterruptHandler<peripherals::USBD>;
-    CLOCK_POWER => usb::vbus_detect::InterruptHandler;
-});
-
-type Drvr = Driver<'static, peripherals::USBD, HardwareVbusDetect>;
-
-#[embassy_executor::task]
-async fn usb_task(mut device: embassy_usb::UsbDevice<'static, Drvr>)
-{
-    device.run().await;
-}    
-
-
-
-
-
-
-
-
-
-
-// func, type test every x seconds with y milli-seconds bw each letter
-//
-async fn type_test() {
-    Timer::after(Duration::from_secs(5)).await;
-    defmt::info!("----xxxx----");
-
-
-    // regarding keycodes
-    // https://www.reddit.com/r/embedded/comments/lz7e14/hid_keyboard_keycodes/
-    // https://gist.github.com/MightyPork/6da26e382a7ad91b5496ee55fdc73db2
-
-    // fuckass task
-    defmt ::info!("typing t");
-    defmt::info!("e");
-    defmt::info!("s");
-    defmt::info!("t");
-
-    defmt::info!("----xxxx----");
+// helper funcs - move to a serivces file
+ pub fn rf_bitrev8(val: u8) -> u8 {
+    val.reverse_bits()
 }
 
+
+
 #[embassy_executor::main]
-async fn main(spawner: Spawner) {
-    // most of this is stolen from https://github.com/embassy-rs/embassy/blob/main/examples/nrf52840/src/bin/usb_hid_keyboard.rs
+async fn main(_spawner: Spawner) {
     let p = embassy_nrf::init(Default::default());
-    
-     // Create the driver
-    let vbus_detect = HardwareVbusDetect::new(Irqs);
-    let driver = Driver::new(p.USBD, Irqs, vbus_detect);
 
-     // Create embassy-usb Config
-    let mut config = Config::new(0xc0de, 0xcafe);
-    config.manufacturer = Some("Embassy");
-    config.product = Some("HID keyboard example");
-    config.serial_number = Some("12345678");
+    // dont use this radio, cant get low levelaccesss to it
+    // let radio = p.RADIO;
 
-    // example code explains why they need to be made static
-    static CONFIG_DESC: StaticCell<[u8; 256]> = StaticCell::new();
-    static BOS_DESC: StaticCell<[u8; 256]> = StaticCell::new();
-    static MSOS_DESC: StaticCell<[u8; 256]> = StaticCell::new();
-    static CONTROL_BUF: StaticCell<[u8; 64]> = StaticCell::new();
+    // use the usntable pac radio instead since i nee low leberl control
+    let radio = embassy_nrf::pac::RADIO;
+    defmt::info!("pac radio setup");
 
-    let mut builder = embassy_usb::Builder::new(
-        driver,
-        config,
-        CONFIG_DESC.init([0; 256]),
-        BOS_DESC.init([0; 256]),
-        MSOS_DESC.init([0; 256]),
-        CONTROL_BUF.init([0; 64]),
-    );
 
-    static HID_STATE: StaticCell<State> = StaticCell::new();
-    let hid_config = HidConfig {
-        report_descriptor: KeyboardReport::desc(),
-        request_handler: None,
-        poll_ms: 10,
-        max_packet_size: 8,
-        hid_subclass: embassy_usb::class::hid::HidSubclass::Boot,
-        hid_boot_protocol: embassy_usb::class::hid::HidBootProtocol::Keyboard,
-    };
+    // docs -> steam controllers use hex ibex as they base address and 0x10 as prefix
+    // todo: move these vals to a dedicated file under radio
+    let sc_base_address: [u8; 4] = *b"ibex"; 
+    let sc_prefix: u8 = 0x10;
 
-    let hid = HidReaderWriter::<_, 1, 8>::new(
-        &mut builder,
-        HID_STATE.init(State::new()),
-        hid_config,
-    );
-    let (_reader, mut writer) = hid.split();
-
-    let usb_device = builder.build();
-    spawner.spawn(usb_task(usb_device)).unwrap();
-
-    loop {
-        Timer::after(Duration::from_secs(5)).await;
-        
-        // move these to a func
-        defmt::info!("Sending 'o'");
-        let report = KeyboardReport { modifier: 0, reserved: 0, leds: 0, keycodes: [0x12, 0, 0, 0, 0, 0] };
-        let _ = writer.write_serialize(&report).await;
-        Timer::after(Duration::from_millis(30)).await;
-        let report = KeyboardReport { modifier: 0, reserved: 0, leds: 0, keycodes: [0; 6] };
-        let _ = writer.write_serialize(&report).await;
-        
-        Timer::after(Duration::from_millis(50)).await;
-
-        defmt::info!("Sending 'k'");
-        let report = KeyboardReport { modifier: 0, reserved: 0, leds: 0, keycodes: [0x0E, 0, 0, 0, 0, 0] };
-        let _ = writer.write_serialize(&report).await;
-        Timer::after(Duration::from_millis(30)).await;
-        let report = KeyboardReport { modifier: 0, reserved: 0, leds: 0, keycodes: [0; 6] };
-        let _ = writer.write_serialize(&report).await;
-    }
+    // next few lines are doing what the rfSetAddr did
+    let rev_base: [u8; 4] = sc_base_address.map(rf_bitrev8); // bit reverse every byte and return as an array
+    let rev_prefix: u8 = rf_bitrev8(sc_prefix); // same for prefix
+    let base0_val = u32::from_be_bytes(rev_base[0..4].try_into().unwrap());
 }
