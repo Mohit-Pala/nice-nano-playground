6aef76f5db46fa9112038d51fee97f61e47e571b
Mohit-Pala <vmohit.pala@gmail.com>
2026-07-26 22:19:39 -0400

    checkpoint: clean up that single file code

--- crates/ropk-fw/src/main.rs
+++ crates/ropk-fw/src/main.rs
@@ -3,6 +3,8 @@
 
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
+use ropk_radio::{sc_radio_config::SteamControllerRadioConfig, sc_radiosetup::ScRadio};
+use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
 
 static RX_BUF: StaticCell<[u8; 100]> = StaticCell::new();
@@ -19,9 +21,20 @@ async fn main(_spawner: Spawner) {
     // let radio = p.RADIO;
     // use the usntable pac radio instead since i nee low leberl control
     let rx_buf: &'static mut [u8; 100] = RX_BUF.init([0; 100]);
-    let mut radio = Radio::new(embassy_nrf::pac::RADIO, rx_buf);
+    let mut radio = ScRadio::new(embassy_nrf::pac::RADIO, rx_buf);
+    radio.config_radio(&SteamControllerRadioConfig::STEAM_CONTROLLER_RADIO_CONFIG);
+    radio.start_sc_radio();
+    defmt::info!("radio started");
 
     loop {
+        if let Some(sc_radio_data) = radio.poll() {
+            defmt::info!("Log start");
+            defmt::info!("CRC OK   : {}", sc_radio_data.crc_ok);
+            defmt::info!("S1/PID   : 0x{:02x}", sc_radio_data.s1_pid);
+            defmt::info!("Length   : {} bytes", sc_radio_data.payload.len());
+            defmt::info!("Payload  : {=[u8]:02x}", sc_radio_data.payload);
+            defmt::info!("Log end");
+        }
         Timer::after(Duration::from_millis(1)).await;
     }
 }
